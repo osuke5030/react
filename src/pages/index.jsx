4ec039c2dd151eee402b377afd8192a5c3b5a222
osuke <o-suke.0108@docomo.ne.jp>
2022-06-08 11:48:22 +0900

    useStateの学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,22 @@ import Head from "next/head";
 import { Footer } from "../components/Footer/Footer";
 import classes from "../styles/Home.module.css";
 import { Header } from "../components/Header/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // 新たな変数を関数内で使う場合、変数がコンポーネント内で定義されていたら関数をコンポーネント外で定義するとエラーとなる
 // コンポーネント内で定義するとレンダリングされると関数も再生成される→解決策useCallBackを使う
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log("aaa");
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    // setCount(function (count) {
+    //   return count + 1;
+    // });
+  };
+
+  console.log(count);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -31,6 +35,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <h1>{count}</h1>
       <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
