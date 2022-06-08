8202a77d57aa72f908cfe1ad1c79b1361e00c7b2
osuke <o-suke.0108@docomo.ne.jp>
2022-06-08 10:55:58 +0900

    useEffectの学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { Footer } from "../components/Footer/Footer";
 import classes from "../styles/Home.module.css";
 import { Header } from "../components/Header/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 // 新たな変数を関数内で使う場合、変数がコンポーネント内で定義されていたら関数をコンポーネント外で定義するとエラーとなる
 // コンポーネント内で定義するとレンダリングされると関数も再生成される→解決策useCallBackを使う
@@ -11,9 +11,17 @@ import { useCallback } from "react";
 export default function Home() {
   const foo = 1;
   const handleClick = useCallback((e) => {
-    console.log(e.target.href);
+    console.log("aaa");
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
