e3b44de18ae45804dcc151682f841352e0ea7967
osuke <o-suke.0108@docomo.ne.jp>
2022-06-09 19:57:48 +0900

    ロジック部分をカスタムフックとしてまとめた

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,41 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import classes from "../styles/Home.module.css";
 import { Footer } from "../components/Footer/Footer";
 import { Main } from "../components/Main/Main";
 import { Header } from "../components/Header/Header";
+import { useCounter } from "../hooks/useCounter";
+import { useInputArray } from "../hooks/useInputArray";
+import { useBgLightBlue } from "../hooks/useBgLightBlue";
 
 export default function Home() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleAdd, handleChange } = useInputArray();
+  useBgLightBlue();
+
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
         <title>About page</title>
       </Head>
 
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <div className={classes.button}>
+        <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示 " : "表示"}</button>
+      </div>
+      <input
+        className={classes.input}
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
+      <button onClick={handleAdd}>追加</button>
+      <ul className={classes.list}>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="about" />
       <Footer />
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,55 +3,17 @@ import Head from "next/head";
 import { Footer } from "../components/Footer/Footer";
 import classes from "../styles/Home.module.css";
 import { Header } from "../components/Header/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "../hooks/useCounter";
+import { useInputArray } from "../hooks/useInputArray";
+import { useBgLightBlue } from "../hooks/useBgLightBlue";
 
 // 新たな変数を関数内で使う場合、変数がコンポーネント内で定義されていたら関数をコンポーネント外で定義するとエラーとなる
 // コンポーネント内で定義するとレンダリングされると関数も再生成される→解決策useCallBackを使う
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-    // setCount(function (count) {
-    //   return count + 1;
-    // });
-  }, [count]);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在しています");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleAdd, handleChange } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={classes.container}>
