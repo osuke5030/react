7ad62450249b16fc2b7239457a1044e5ad84f2d5
osuke <o-suke.0108@docomo.ne.jp>
2022-06-10 13:59:53 +0900

    エラー時、データがない時、ローディング時の処理の追加

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,48 @@
+import { useState, useCallback, useEffect } from "react";
+
+const Posts = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("エラーが発生したため、データの取得に失敗しました");
+      }
+      const json = await res.json();
+      setPosts(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  if (loading) {
+    return <div>ローディング中です</div>;
+  }
+
+  if (error) {
+    return <div>{Error.message}</div>;
+  }
+
+  if (posts.length === 0) {
+    return <div>データが空です</div>;
+  }
+  return (
+    <div>
+      <ol>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default Posts;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,22 +2,12 @@ import Head from "next/head";
 import classes from "../styles/Home.module.css";
 import { Header } from "../components/Header/Header";
 import { useState, useCallback, useEffect } from "react";
+import Posts from "../components/Posts";
 
 // 新たな変数を関数内で使う場合、変数がコンポーネント内で定義されていたら関数をコンポーネント外で定義するとエラーとなる
 // コンポーネント内で定義するとレンダリングされると関数も再生成される→解決策useCallBackを使う
 
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
+const Home = () => {
   return (
     <div className={classes.container}>
       <Head>
@@ -25,13 +15,7 @@ const Home = (props) => {
       </Head>
       <Header />
 
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
